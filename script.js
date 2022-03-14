e56f2231b8f659c8146f033acf70858c24ed765d
Kojech <101091237+Kojech@users.noreply.github.com>
2022-03-14 09:28:25 +0100

    fixed script

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-window.onload(function(){
-    const button = document.querySelector('#java');
+const button = document.querySelector('#java');
 
-    button.addEventListener('click', function() {
-        alert('Nacisnąłeś batona');
-    });
+ button.addEventListener('click', function() {
+    alert('Nacisnąłeś batona');
 });
